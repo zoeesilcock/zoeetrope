247da617870e3f011b5e18dc1e9f1408d0d0f4cb
Zoee Silcock <mrzoee@gmail.com>
2014-01-12 12:15:59 +0100

    improve routing for contact page

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Zoeetrope::Application.routes.draw do
-  get "home/index"
+  get 'home/index'
   resources :projects, only: [:index, :show]
-  get "contact/index"
+  get 'contact', to: 'contact#index'
   root 'home#index'
 end
