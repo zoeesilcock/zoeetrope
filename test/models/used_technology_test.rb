2cea77948b2825dfa73ca6c3749727bb39a57ead
Zoee Silcock <mrzoee@gmail.com>
2014-05-10 21:06:36 +0200

    A couple of minor syntactical changes

--- test/models/used_technology_test.rb
+++ test/models/used_technology_test.rb
@@ -2,9 +2,9 @@ require 'test_helper'
 
 describe UsedTechnology do
   it 'gets added at the end of the list' do
-    project = Project.find 1
+    project = Project.find(1)
     technology = Technology.create(title: 'Ruby')
-    used_technology = UsedTechnology.create(project: project, technology: used_technology)
+    used_technology = project.used_technologies.create(technology: technology)
 
     assert_equal used_technology.sort_order, project.used_technologies.count
   end
